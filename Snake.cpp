05f3bbfd021f9f1b23f0a2d14798a0c826c5348a
Omer Drori <73065220+Omerd05@users.noreply.github.com>
2023-09-15 11:52:05 +0300

    Update and rename Tetris.cpp to Snake.cpp
    Pretty major updates in the implementation of the game.
    First thing, the game isn't "lagging" anymore.
    Second, the implementation is smoother and wiser.

--- Snake.cpp
+++ Snake.cpp
@@ -16,84 +16,82 @@ const int N = 15;
 const int M = 25;
 
 vector<vector<char>> gameMap(N, vector<char>(M));
-uniform_int_distribution<int> xGen(2, 23);
-uniform_int_distribution<int> yGen(2, 13);
+uniform_int_distribution<int> xGen(5, 95);
+uniform_int_distribution<int> yGen(5, 25);
 random_device rd;
 mt19937 rng(rd());
 
 deque<pair<int, int>> cords;
 set<pair<int, int>> occupied;
-
+pair<int, int> apple;
 
 pair<int, int> randomDot() {
-	return { yGen(rng),xGen(rng) };
+	return { xGen(rng),yGen(rng) };
+}
+
+void changeChar(int x, int y, char ch) {
+	cout << "\033[" << y << ";" << x << "H";
+	cout << ch;
+}
+
+void spawnApple() {
+	cout << "\033[31m";
+	auto it = randomDot();
+	while (occupied.count(it))it = randomDot();
+	apple = it;
+	changeChar(apple.first, apple.second, '0');
+	std::cout << "\033[0m";
+}
+
+void printBorder() {
+	for (int i = 0; i < 100; i++) {
+		changeChar(i, 0, 'Q');
+		changeChar(i, 30, 'Q');
+	}
+	for (int i = 0; i < 30; i++) {
+		changeChar(0, i, 'Q');
+		changeChar(100, i, 'Q');
+	}
 }
 
 void init() {
-	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) {
-			gameMap[i][j] = '*';
-		}
-	}
-	for (int i = 0; i < N; i++) {
-		gameMap[i][0] = '|',gameMap[i][M - 1] = '|';
-	}
-	for (int i = 0; i < M; i++) {
-		gameMap[0][i] = '_',gameMap[N - 1][i] = '-';
-	}
 	int x, y;
 	auto it = randomDot();
-	y = it.first;
-	x = it.second;
-	cords.push_front({ y,x });
-	occupied.insert({ y,x });
-	gameMap[y][x] = 'X';
+	x = it.first;
+	y = it.second;
+	cords.push_front({ x,y });
+	occupied.insert({ x,y });
+	changeChar(x,y,'X');
 
-	it = randomDot();
-	while(it.first == y && it.second == x)it = randomDot();
-	gameMap[it.first][it.second] = 'O';
-}
 
-void printMap() {
-	cout << "\033[2J\033[1;1H";
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout << gameMap[i][j];
-		}
-		cout << '\n';
-	}
-	cout << "Score : " << cords.size();
+	spawnApple();
+	printBorder();
 }
 
 bool update(pair<int,int> p) {
 	auto it = cords.front();
 	pair<int,int> face = { it.first + p.first,it.second + p.second };
-	if (occupied.count(face) || (face.first == 0 || face.first == N-1) || (face.second == 0 || face.second == M-1))return 0;
+	if (occupied.count(face) || (face.first == 0 || face.first > 99) || (face.second == 0 || face.second >= 30))return 0;
 	
 	cords.push_front(face);
 	occupied.insert(face);
+
 	
-	if (gameMap[face.first][face.second] == 'O') {
-		pair<int, int> apl = randomDot();
-		int t = 0;
-		while (occupied.count(apl)) {
-			apl = randomDot();
-			if (t++ >= 100)return 0;
-		}
-		gameMap[apl.first][apl.second] = 'O';
+	if (face == apple) { //double check correctness
+		spawnApple();
 	}
 	else {
-		gameMap[cords.back().first][cords.back().second] = '*';
+		changeChar(cords.back().first, cords.back().second,' ');
 		occupied.erase(cords.back());
 		cords.pop_back();
 	}
-	gameMap[face.first][face.second] = 'X';
-	printMap();
+	changeChar(face.first, face.second, 'X');
 	return 1;
 }
 
 int main()
 {
+	ios::sync_with_stdio(0); cout.tie(0);
 	init();
 
 	char input;
@@ -105,7 +103,7 @@ int main()
 	auto speedT = steady_clock::now();
 	auto fpsT = steady_clock::now();
 
-	int pace = 500;
+	int pace = 100;
 
 	while (running) {
 		if (duration_cast<milliseconds>(steady_clock::now() - speedT).count() >= pace/sqrt(cords.size())) {
@@ -116,28 +114,27 @@ int main()
 					pair<int, int> tempo = { op.first,op.second };
 					switch (input) {
 					case 72: //Up
-						op = { -1,0 };
-						break;
-					case 75: //Left
 						op = { 0,-1 };
 						break;
+					case 75: //Left
+						op = { -1,0 };
+						break;
 					case 80: //Down
-						op = { 1,0 };
+						op = { 0,1};
 						break;
 					case 77: //Right
-						op = { 0,1 };
+						op = { 1,0 };
 						break;
 					}
 					if (tempo.first + op.first == 0 && tempo.second + op.second == 0)op = tempo;
 				}
 			}
 			if(!update(op))return 0;
-			printMap();
 			speedT = steady_clock::now();
 		}
 		this_thread::sleep_for(milliseconds(10));
 	}
 
-	cout << "\033[2J\033[1;1H";
+	system("cls");
 	cout << "Game Over"; cin;
 }
